8fd22d131a0e4766569ed29bd8a85e3ca2507c0f
FRANK <frankmascco@gmail.com>
2024-11-22 10:52:50 -0500

    Primer commit

---
+++ src/main/java/com/sistema/hotel/SistemaHotelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHotelBackendApplication.class, args);
+	}
+
+}
